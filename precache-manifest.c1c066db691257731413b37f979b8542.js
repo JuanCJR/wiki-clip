692cece03d5b1b3cd2a7033dff2a994217d8b6b1
MrReckless <juancarlo14071@gmail.com>
2019-08-19 17:06:53 -0400

    Updates

---
+++ precache-manifest.c1c066db691257731413b37f979b8542.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482336f97c1df895c6d64778bfb7404a",
+    "url": "/wiki-clip/index.html"
+  },
+  {
+    "revision": "3ebc7508ba1dfc21069d",
+    "url": "/wiki-clip/static/css/main.4fd25d6e.chunk.css"
+  },
+  {
+    "revision": "c55ed37633b717f8fbe2",
+    "url": "/wiki-clip/static/js/2.b299884f.chunk.js"
+  },
+  {
+    "revision": "3ebc7508ba1dfc21069d",
+    "url": "/wiki-clip/static/js/main.e02aaa21.chunk.js"
+  },
+  {
+    "revision": "dab3c94c53e99ccd4d76",
+    "url": "/wiki-clip/static/js/runtime~main.eb8dd91e.js"
+  }
+]);
