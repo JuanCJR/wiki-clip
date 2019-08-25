587c9161399335fa53dea6c82fbf71c81e4b838c
MrReckless <juancarlo14071@gmail.com>
2019-08-25 17:52:59 -0400

    conexion backend

--- src/components/KnowedgeBase/CreateKBItem.js
+++ src/components/KnowedgeBase/CreateKBItem.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react'
-import axios from 'axios' //Biblioteca que me permite realizar peticiones HTTP
 export default class CreateKBItem extends Component {
     
-    //llama esta funcion cuando el componente ha sido montado
-   async componentDidMount(){
-
-        await axios.get
-    }
+  
     
     render() {
         return (
             <div>
-                
+                                
             </div>
         )
     }
--- src/test/test.js
+++ src/test/test.js
@@ -1,16 +1,112 @@
 import React, { Component } from 'react'
 import Button from 'react-bootstrap/Button'
-import NavBar from '../components/NavBar'
+import Form from 'react-bootstrap/Form'
 import Card from 'react-bootstrap/Card'
-import datos from '../components/KnowedgeBase/itemBD';
-
+import axios from 'axios'
 export default class test extends Component {
 
-    
+
+    state ={
+            Aplicativo:"",
+            Descripcion:"",
+            Tipo:"",
+            Contenido:"",
+            Autor:""
+    }
+    onChangeDescripcion = (e) =>{
+        this.setState({
+            Descripcion: e.target.value
+        });
+        console.log(e.target.value);
+    }
+
+
+    onChangeAplicativo = (e) =>{
+        this.setState({
+            Aplicativo: e.target.value
+        });
+        console.log(e.target.value);
+    }
+
+    onChangeTipo = (e) =>{
+        this.setState({
+            Tipo: e.target.value
+        });
+        console.log(e.target.value);
+    }
+
+    onChangeContenido = (e) =>{
+        this.setState({
+            Contenido: e.target.value
+        });
+        console.log(e.target.value);
+    }
+
+    onChangeAutor = (e) =>{
+        this.setState({
+            Autor: e.target.value
+        });
+        console.log(e.target.value);
+    }
+
+    onSubmit = async (e) =>{
+        e.preventDefault();
+        const res = await axios.post('http://localhost:8080/api/KnowedgeBase',this.state);
+        console.log(res);
+
+      
+    }
+
     render() {
         return (
-      
-            <div></div>
+            <Card className="container w-50 pb-3">
+
+                
+                <Form onSubmit={this.onSubmit}>
+                    <Form.Group controlId="Aplicativo">
+                        <Form.Label>Aplicativo</Form.Label>
+                        <Form.Control as="select" 
+                        placeholder="Seleccione un Aplcativo"
+                         onChange={this.onChangeAplicativo}
+                       >
+                           <option>Seleccione un Aplicativo</option>
+                            <option>GoAnywhere</option>
+                            <option>Aldon</option>
+                            <option>Enforcive</option>
+                            <option>Presto</option>
+                        </Form.Control>
+
+                    </Form.Group>
+                    <Form.Group controlId="Descripcion">
+                        <Form.Label>Descripcion</Form.Label>
+                        <Form.Control placeholder="Ingrese una Descripcion"
+                        onChange={this.onChangeDescripcion}></Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="Tipo">
+                        <Form.Label>Tipo de documento</Form.Label>
+                        <Form.Control as="select"
+                        onChange={this.onChangeTipo}>
+                            <option>Selecione el Tipo de Documento</option>
+                            <option>Actualizacion</option>
+                            <option>Correcion de errores</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="Contenido" 
+                    onChange={this.onChangeContenido}>
+                       <Form.Label>Contenido</Form.Label>
+                       <Form.Control as="textarea" rows="5"/>
+                    </Form.Group>
+                    <Form.Group controlId="Autor"
+                    onChange={this.onChangeAutor}>
+                        <Form.Label>Autor</Form.Label>
+                        <Form.Control></Form.Control>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Enviar  
+            </Button>
+                </Form>
+            </Card>
+
         )
     }
 }
